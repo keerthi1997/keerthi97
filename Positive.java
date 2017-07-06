b845d7b55605fd4b0215df35b42ccc67009ff36f
keerthi1997 <keerthana.vjps@gmail.com>
2017-07-06 14:52:16 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Positive{
+public static void main(String[] args)
+{
+int n;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+n=Integer.parseInt(br.readLine());
+if(n<o)
+System.out.println("n is negative");
+else if(n>0)
+System.out.println("n is positive");
+else
+System.out.println("n is zero");
+}
+}
